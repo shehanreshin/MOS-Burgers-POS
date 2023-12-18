0b15860d47d21bda381c56719146ae947321d751
shehanreshin <asp.shehanreshin@gmail.com>
2023-12-18 16:46:41 +0530

    Add session logic to a seperate JS file

--- asset/js/home.js
+++
@@ -1,3 +0,0 @@
-if (CryptoJS.SHA256(localStorage.getItem('loggedIn')).toString(CryptoJS.enc.Base64) != "6PAQ3WdNc2xsyjy3S07LnO273PrMSPydhXUORe6pelI=") {
-    window.location.replace('index.html');
-}
---
+++ asset/js/session.js
@@ -0,0 +1,17 @@
+fetch(new Request("./asset/data/user_credentials.json"))
+    .then(response => {
+        return response.json();
+    })
+    .then(json => {
+        var users = json.users;
+        var isPasswordFound = false;
+        users.forEach(user => {
+            if (CryptoJS.SHA256(localStorage.getItem('loggedIn')).toString(CryptoJS.enc.Base64) === user.password) {
+                isPasswordFound = true;
+            }
+        });
+        if (!isPasswordFound) {
+            window.location.replace('index.html');
+        }
+    });
+
