f9ebec5469c97a32204dd98d31b2038bad86107b
shehanreshin <asp.shehanreshin@gmail.com>
2023-12-19 22:39:38 +0530

    Add fries data to items JSON and display them when fries is clicked on in navbar. Additionally, change the color of the selected item type in the navbar to purple

--- asset/js/home.js
+++ asset/js/home.js
@@ -1,7 +1,8 @@
 let itemsSection = document.getElementById('items-section');
-let items;
-let burgers;
-let submarines;
+let items, burgers, submarines, fries, pasta, beverages;
+let navBurgers = document.getElementById('nav-burgers');
+let navSubmarines = document.getElementById('nav-submarines');
+let navFries = document.getElementById('nav-fries');
 
 function generateBurgers() {
     var burgerCardsHTML = "";
@@ -33,7 +34,6 @@ function generateBurgers() {
 
 function generateSubmarines() {
     var submarineCardsHTML = "";
-
     submarines.forEach(submarine => {
         submarineCardsHTML += `<div class="col-lg-4 col-md-6 col-sm-6">
             <div class="card w-100 h-100" id="${submarine.code}">
@@ -60,6 +60,34 @@ function generateSubmarines() {
     itemsSection.innerHTML = submarineCardsHTML;
 }
 
+function generateFries() {
+    var friesCardsHTML = "";
+    fries.forEach(friedItem => {
+        friesCardsHTML += `<div class="col-lg-4 col-md-6 col-sm-6">
+            <div class="card w-100 h-100" id="${friedItem.code}">
+                <img src="${friedItem.image}" class="card-img-top w-100 h-50 object-fit-cover"
+                    alt="burger">
+                <div class="card-body d-flex flex-column justify-content-between">
+                    <div>
+                        <h6 class="card-title mb-2">${friedItem.name}</h6>
+                        <h6 class="fw-bolder">Rs. ${(friedItem.price).toFixed(2)}</h6>
+                    </div>
+
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="expiry-status color-text-danger">
+                            
+                        </div>
+                        <div><button
+                                class="btn color-bg-primary color-txt-white pe-4 ps-4">Add</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`
+    });
+    itemsSection.innerHTML = friesCardsHTML;
+}
+
 fetch(new Request("./asset/data/items.json"))
     .then(response => {
         return response.json();
@@ -68,11 +96,30 @@ fetch(new Request("./asset/data/items.json"))
         items = json.items;
         burgers = items.burgers;
         submarines = items.submarines;
+        fries = items.fries;
         generateBurgers();
     });
 
-document.getElementById('nav-burgers').addEventListener('click', () => generateBurgers());
-document.getElementById('nav-submarines').addEventListener('click', () => {
-    document.getElementById('nav-submarines').style;
+function resetActiveNavSelection() {
+    navBurgers.classList.remove('color-txt-primary');
+    navSubmarines.classList.remove('color-txt-primary');
+    navFries.classList.remove('color-txt-primary');
+}
+
+navBurgers.addEventListener('click', () => {
+    resetActiveNavSelection();
+    navBurgers.classList.add('color-txt-primary');
+    generateBurgers();
+});
+
+navSubmarines.addEventListener('click', () => {
+    resetActiveNavSelection();
+    navSubmarines.classList.add('color-txt-primary');
     generateSubmarines();
+});
+
+navFries.addEventListener('click', () => {
+    resetActiveNavSelection();
+    navFries.classList.add('color-txt-primary');
+    generateFries();
 });
