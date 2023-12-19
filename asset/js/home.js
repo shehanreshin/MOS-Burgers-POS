91b63ec7faaa0cc185b76f2e71794364822a8053
shehanreshin <asp.shehanreshin@gmail.com>
2023-12-19 23:02:41 +0530

    Add pasta data to items JSON and display them when pasta is clicked on in navbar.

--- asset/js/home.js
+++ asset/js/home.js
@@ -1,8 +1,10 @@
 let itemsSection = document.getElementById('items-section');
 let items, burgers, submarines, fries, pasta, beverages;
-let navBurgers = document.getElementById('nav-burgers');
-let navSubmarines = document.getElementById('nav-submarines');
-let navFries = document.getElementById('nav-fries');
+let navBurgers = document.getElementById('nav-burgers'),
+    navSubmarines = document.getElementById('nav-submarines'),
+    navFries = document.getElementById('nav-fries'),
+    navPasta = document.getElementById('nav-pasta');
+
 
 function generateBurgers() {
     var burgerCardsHTML = "";
@@ -88,15 +90,48 @@ function generateFries() {
     itemsSection.innerHTML = friesCardsHTML;
 }
 
+function generatePasta() {
+    var pastaCardsHTML = "";
+    pasta.forEach(pastaItem => {
+        pastaCardsHTML += `<div class="col-lg-4 col-md-6 col-sm-6">
+            <div class="card w-100 h-100" id="${pastaItem.code}">
+                <img src="${pastaItem.image}" class="card-img-top w-100 h-50 object-fit-cover"
+                    alt="burger">
+                <div class="card-body d-flex flex-column justify-content-between">
+                    <div>
+                        <h6 class="card-title mb-2">${pastaItem.name}</h6>
+                        <h6 class="fw-bolder">Rs. ${(pastaItem.price).toFixed(2)}</h6>
+                    </div>
+
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="expiry-status color-text-danger">
+                            
+                        </div>
+                        <div><button
+                                class="btn color-bg-primary color-txt-white pe-4 ps-4">Add</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`
+    });
+    itemsSection.innerHTML = pastaCardsHTML;
+}
+
+function intializeItemArrays(items) {
+    burgers = items.burgers;
+    submarines = items.submarines;
+    fries = items.fries;
+    pasta = items.pasta;
+}
+
 fetch(new Request("./asset/data/items.json"))
     .then(response => {
         return response.json();
     })
     .then(json => {
         items = json.items;
-        burgers = items.burgers;
-        submarines = items.submarines;
-        fries = items.fries;
+        intializeItemArrays(items);
         generateBurgers();
     });
 
@@ -104,6 +139,7 @@ function resetActiveNavSelection() {
     navBurgers.classList.remove('color-txt-primary');
     navSubmarines.classList.remove('color-txt-primary');
     navFries.classList.remove('color-txt-primary');
+    navPasta.classList.remove('color-txt-primary');
 }
 
 navBurgers.addEventListener('click', () => {
@@ -122,4 +158,10 @@ navFries.addEventListener('click', () => {
     resetActiveNavSelection();
     navFries.classList.add('color-txt-primary');
     generateFries();
+});
+
+navPasta.addEventListener('click', () => {
+    resetActiveNavSelection();
+    navPasta.classList.add('color-txt-primary');
+    generatePasta();
 });
